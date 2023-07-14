19ef71483347f6e27e64141cc89af4e5f12b5512
dansantos2 <danielsana60@gmail.com>
2023-07-14 18:11:16 +0300

    commit 2

---
+++ scripts/forecast.js
@@ -0,0 +1,39 @@
+const key = '8kB8x6Qt6adxMuCTWinmnM9IH8ECeG1F'
+
+//get weather information
+const getWeather = async (id) => {
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query)
+
+    const data = await response.json();
+
+    return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query)
+
+    const data = await response.json();
+
+    return data[0];
+    
+};
+
+getCity('nairobi')
+    .then(data => {
+        return getWeather(data.Key)
+    }).then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err));
+
+// getWeather('224758')
